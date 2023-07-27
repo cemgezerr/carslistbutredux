6876db14ad3113dea0eb407a9fd43a1b641b1135
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-07-27 17:35:10 +0300

    car slice

--- src/store/slices/CarSlice.js
+++ src/store/slices/CarSlice.js
@@ -4,28 +4,26 @@ const carsSlice = createSlice({
   name: 'cars',
   initialState: {
     searchTerm: '',
-    cars: [],
+    data: [],
   },
   reducers: {
     changeSearchTerm(state, action) {
       state.searchTerm = action.payload;
     },
     addCar(state, action) {
-      // Assumption:
-      // action.payload === { name: 'ab', cost: 140 }
-      state.cars.push({
+
+      state.data.push({
         name: action.payload.name,
         cost: action.payload.cost,
         id: nanoid(),
       });
     },
     removeCar(state, action) {
-      // Assumption:
-      // action.payload === the id of the car we want to remove
-      const updated = state.cars.filter((car) => {
+
+      const updated = state.data.filter((car) => {
         return car.id !== action.payload;
       });
-      state.cars = updated;
+      state.data = updated;
     },
   },
 });
